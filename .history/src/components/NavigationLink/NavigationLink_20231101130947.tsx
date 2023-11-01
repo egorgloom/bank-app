72f8d64325fe0f2667e89d8867a2a13ca32a8c3c
EgorGL00MI <gitstage0@yandex.ru>
2023-11-01 15:33:38 +0300

    feat: add media 480px

---
+++ .history/src/components/NavigationLink/NavigationLink_20231101130947.tsx
@@ -0,0 +1,88 @@
+import { FC } from 'react';
+
+import { NavLink } from "react-router-dom";
+
+interface INavigationLink {
+
+    active?: boolean,
+    setActive?: any,
+
+}
+
+
+
+const NavigationLink: FC<INavigationLink> = ({active, setActive}) => {
+
+    const pages = ['Summary', 'Cards', 'Activity', 'Recipients', 'Help Center', 'Earn Gifts']
+
+    return (
+        <>
+
+            <nav className={active ? 'navContainer active' : 'navContainer'} onClick={()=>setActive(false)}>
+                <ul className={active ? 'navLink active' : 'navLink'} >
+                    {pages.map((page: string) =>                     
+                    <li>
+                        <NavLink to={`/${page}`}
+                            style={({ isActive }) => ({
+                                color: isActive ? "black" : 'rgb(184,182,184)',
+                            })}
+                        >
+                            {page}
+                        </NavLink>
+                    </li>)}
+                    {/* <li>
+                        <NavLink to={'/summary'}
+                            style={({ isActive }) => ({
+                                color: isActive ? "black" : 'rgb(184,182,184)',
+                            })}
+                        >
+                            Summary
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to={'/cards'}
+                            style={({ isActive }) => ({
+                                color: isActive ? 'black' : 'rgb(184,182,184)',
+                            })}>
+                            Cards
+                        </NavLink></li>
+                    <li>
+                        <NavLink to={'/activity'}
+                            style={({ isActive }) => ({
+                                color: isActive ? 'black' : 'rgb(184,182,184)',
+                            })}
+                        >
+                            Activity
+                        </NavLink></li>
+                    <li>
+                        <NavLink to={'/recipients'}
+                            style={({ isActive }) => ({
+                                color: isActive ? 'black' : 'rgb(184,182,184)',
+                            })}
+                        >
+                            Recipients
+                        </NavLink></li>
+                    <li>
+                        <NavLink to={'/helpCenter'}
+                            style={({ isActive }) => ({
+                                color: isActive ? 'black' : 'rgb(184,182,184)',
+                            })}
+                        >
+                            Help Center
+                        </NavLink></li>
+                    <li>
+                        <NavLink to={'/earnGifts'}
+                            style={({ isActive }) => ({
+                                color: isActive ? 'black' : 'rgb(184,182,184)',
+                            })}
+                        >
+                            Earn Gifts
+                        </NavLink></li> */}
+                </ul>
+            </nav>
+        </>
+
+    );
+};
+
+export default NavigationLink;
